f491bdc713e96093b4921a660d21bc84c4871522
Miguel Macario <78888022+MiguelMaca@users.noreply.github.com>
2025-08-17 13:43:59 -0600

    BTreeNode
    Creacion de la Clase BTreeNode

---
+++ proyecto1.ts
@@ -0,0 +1,105 @@
+//Proyecto 1 - Estructura de Datos 1
+//Miguel Macario
+//Jason Gutierrez
+//Abel de Leon
+
+interface Proveedor {
+    id: number;
+    nombre: string;
+    servicio: string;
+    calificacion: number;
+}
+
+class BTreeNode {
+    t: number; 
+    proveedores: Proveedor[] = [];
+    hijos: BTreeNode[] = [];
+    esHoja: boolean;
+
+    constructor(t: number, esHoja: boolean) {
+        this.t = t;
+        this.esHoja = esHoja;
+    }
+
+    recorrer(): Proveedor[] {
+        let resultado: Proveedor[] = [];
+        let i: number;
+        for (i = 0; i < this.proveedores.length; i++) {
+            if (!this.esHoja) {
+                resultado.push(...this.hijos[i].recorrer());
+            }
+            resultado.push(this.proveedores[i]);
+        }
+        if (!this.esHoja) {
+            resultado.push(...this.hijos[i].recorrer());
+        }
+        return resultado;
+    }
+
+    buscarPorServicio(servicio: string): Proveedor[] {
+        let resultados: Proveedor[] = [];
+        for (let prov of this.proveedores) {
+            if (prov.servicio.toLowerCase() === servicio.toLowerCase()) {
+                resultados.push(prov);
+            }
+        }
+        if (!this.esHoja) {
+            for (let hijo of this.hijos) {
+                resultados.push(...hijo.buscarPorServicio(servicio));
+            }
+        }
+        return resultados;
+    }
+
+    insertarNoLleno(proveedor: Proveedor) {
+        let i = this.proveedores.length - 1;
+        if (this.esHoja) {
+            while (i >= 0 && proveedor.id < this.proveedores[i].id) {
+                i--;
+            }
+            this.proveedores.splice(i + 1, 0, proveedor);
+        } else {
+            while (i >= 0 && proveedor.id < this.proveedores[i].id) {
+                i--;
+            }
+            if (this.hijos[i + 1].proveedores.length === (2 * this.t - 1)) {
+                this.dividirHijo(i + 1, this.hijos[i + 1]);
+                if (proveedor.id > this.proveedores[i + 1].id) {
+                    i++;
+                }
+            }
+            this.hijos[i + 1].insertarNoLleno(proveedor);
+        }
+    }
+
+    dividirHijo(i: number, y: BTreeNode) {
+        const z = new BTreeNode(y.t, y.esHoja);
+
+        // Guardar el valor medio antes de cortar
+        const valorMedio = y.proveedores[this.t - 1];
+
+        // Pasar las claves a z
+        for (let j = 0; j < this.t - 1; j++) {
+            z.proveedores.push(y.proveedores[j + this.t]);
+        }
+
+        // Pasar hijos si no es hoja
+        if (!y.esHoja) {
+            for (let j = 0; j < this.t; j++) {
+                z.hijos.push(y.hijos[j + this.t]);
+            }
+        }
+
+        // Reducir y a t-1 claves
+        y.proveedores.length = this.t - 1;
+        if (!y.esHoja) {
+            y.hijos.length = this.t;
+        }
+
+        // Insertar z como nuevo hijo
+        this.hijos.splice(i + 1, 0, z);
+        // Insertar el valor medio en el nodo actual
+        this.proveedores.splice(i, 0, valorMedio);
+    }
+}
+
